cde75b52fb8f69da3f83e41649d46f19af6f6dfc
jvesguerra <jvesguerra@up.edu.ph>
2022-12-19 23:10:04 +0800

    Feat: Board Functions

--- a.py
+++ a.py
@@ -12,13 +12,57 @@ screen.fill(BG_COLOR)
 
 class Board:
     def __init__(self):
-        self.squares = np.zeros((ROWS,COLS))
+        self.squares = np.zeros((ROWS,COLS))    # initialize values to 0
+        self.empty_sqrs = self.squares # list of squares
+        self.marked_sqrs = 0
     
     def mark_sqr(self,row,col,player):
         self.squares[row][col] = player
+        self.marked_sqrs += 1   # to know when board is full
 
     def empty_sqr(self,row,col):
         return self.squares[row][col] == 0
+
+    def isfull(self):
+        return self.marked_sqrs == 9
+
+    def isempty(self):
+        return self.marked_sqrs == 0
+
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_sqr(row,col) == 0:
+                    empty_sqrs.append((row,col))
+        return empty_sqrs
+
+    # return 0 if no one won yet
+    # return 1 if p1 wins
+    # return 2 if p2 wins
+    def final_state(self):
+        # vertical wins
+        for col in range(COLS):
+            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:   # not equal to 0 means it is not empty   
+                return self.squares[0][col]     # this is the player number
+        
+        # horizonal wins
+        for row in range(ROWS):
+            if self.squares[row][0] == self.squares[row][0] == self.squares[row][0] != 0:   # not equal to 0 means it is not empty   
+                return self.squares[row][0]     # this is the player number
+
+        # \ diagonal win
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:   # not equal to 0 means it is not empty 
+            return self.squares[1][1]
+
+        # / diagonal win
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:   # not equal to 0 means it is not empty 
+            return self.squares[1][1]
+        
+        # no win
+        return 0
+
+
 class Game:
     def __init__(self):
         self.board = Board()
